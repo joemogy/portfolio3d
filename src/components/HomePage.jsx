699e15357bdc1f52cf22f18fefeae79580714994
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-09 22:36:35 -0500

    added styling to About & Backgrounds & ad three.js

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,95 +1,61 @@
-import React, { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import "../css/Homepage.css";
-
-
+import React from "react";
+import {Links} from './Backgrounds';
+import frontImg from './../resources/desk.png';
+import { Canvas } from "@react-three/fiber";
+import { Sphere, MeshDistortMaterial, SpotLight } from "@react-three/drei";
+import {HandleSwipes, ContactClick} from './About';
 export default function HomePage() {
-    const particlesInit = useCallback(async engine => {
-        await loadFull(engine);
-    }, [])
+  return (
+    <>
+    <div className="main">
+    <Links />
+        <h1 className="logo">
+          Dev<span>Joseph <Mogavero></Mogavero></span>
+        </h1>
+        <nav>
+          <ul>
+            <a href="#About" className="icons">About <i class="fa-solid fa-user-tie"></i></a>
+            <a href="#About" onClick={HandleSwipes} className="icons">Skills <i class="fa-solid fa-screwdriver-wrench"></i>
+            </a>
+            <a href="#About" onClick={ContactClick} className="icons">Contacts <i class="fa-solid fa-mobile-button"></i>
+            </a>
+            <a href="/" className="icons">Projects <i class="fa-solid fa-list-check"></i>
+            </a>
+            <a href="/" className="icons">Services <i class="fa-solid fa-server"></i>
+            </a>
+          </ul>
+        </nav>
+        <div className="content">
+          <h2 className="arrowL arrow"> &#x250C; </h2>
+          <br />
+          <h4>Hello, I'm </h4>
+          <h1>Joseph Mogavero</h1>
+          <h3>I'm a Developer!</h3>
+          <h2 className="arrowR arrow"> &#x2518; </h2>
+          <br />
+          <a href="/" download className="download">Download CV<i class="fa-solid fa-download"></i></a>
+        </div>
 
-    const particlesLoaded = useCallback(async engine => {
-        console.log(engine)
-        await loadFull(engine)
-    })
-
-    return (
-        <>
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={{
-                    background: {
-                        color: {
-                            value: "#202020",},},
-                    fpsLimit: 60,
-                    interactivity: {
-                        events: {
-                            onClick: {
-                                enable: true,
-                                mode: "push",},
-                            onHover: {
-                                enable: true,
-                                mode: "grab",},},
-                        modes: {
-                            push: {
-                                quantity: 3,},
-                            grab: {
-                                duration: 0.6,
-                                distance: 199,},},},
-                        particles: {
-                            color: {
-                                value: "#ffffff",},
-                        links: {
-                            color: {
-                                value: "#ff1122",},
-                            enable: true,
-                            width: 1,
-                            opacity: 1,
-                            distance: 166,},
-                        move: {
-                            enable: true,
-                            direction: "none",
-                            outModes: {
-                                default: "bounce",},
-                            random: false,
-                            speed: 2,
-                            straight: false,},
-                        collisions: {
-                            enable: true, },
-                        number: {
-                            value: 33,
-                            density: {
-                                enable: true,
-                                area: 800,},},
-                        shape: {
-                            type: "torus",
-                            type: "circle",},
-                        size: {
-                            min: 1,
-                            max: 6,},},
-                        detectRetina: true,}}/>
-        
-
-<div className="main">
-    <nav>
-        <h1 className="logo">Dev <span>Joseph Mogavero</span></h1>
-            <ul>
-                <li className="icons">Projects</li>
-                <li className="icons">About</li>
-                <li className="icons">Websites</li>
-                <li className="icons">Work</li>
-                <li className="icons">Certificates</li>
-                <li className="icons">Contact</li>
-            </ul>
-     </nav>
-    <div className="content">
-        <h2 className="arrowR "></h2>
+      <div className="imges">
+        <Canvas style={{position: 'absolute', width: '1450px', height: '680px', left: '27%', top: '7%'}}>
+          <mesh> 
+          <ambientLight intensity={1.2} />
+          <SpotLight intensity={3} position={[-30, 0, 0]} />
+          <directionalLight intensity={1} color="0xffffff"/>
+        <Sphere visible scale={2} args={[1.2, 500, 600]}>
+        <MeshDistortMaterial
+        style={{height: '670px'}}
+        color={'#d95b00'}
+        roughness={0.4}
+        distort={0.3}
+        speed={1.5}
+        />
+        </Sphere>
+        </mesh>
+        </Canvas>
+           <img src={frontImg} alt="Front Img" className="frontimg" />
+      </div>
     </div>
-</div>
-
-</>
-    );
+    </>
+  );
 }
